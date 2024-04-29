8b33f6b350a9f485bc35a99428370083f7395428
Rapiwash <sistemarapiwash@gmail.com>
2024-04-28 21:54:28 -0500

    update - add personal, asistencia tipo de gasto, reporte anulado, cuadre diario, news consultas cuadre , pagos, gastos , anulaciones + correcciones

--- src/components/PRIVATE/InputSelectedPrenda/InputSelectedPrenda.jsx
+++ src/components/PRIVATE/InputSelectedPrenda/InputSelectedPrenda.jsx
@@ -35,7 +35,7 @@ const InputSelectedPrenda = ({ listenClick, tabI, disabled }) => {
       const categoria = mapeoCategorias[servicio.idCategoria];
       // Excluir si el servicio es "Delivery" y su categoría es de nivel "primario" o si el estado es false
       return (
-        !(servicio.nombre === "Delivery" && categoria.nivel === "primario") &&
+        !(servicio.nombre === "Delivery" && categoria?.nivel === "primario") &&
         servicio.estado
       );
     });
@@ -51,7 +51,11 @@ const InputSelectedPrenda = ({ listenClick, tabI, disabled }) => {
       };
     });
 
-    setData(serviciosDB);
+    const infoOrdenada = serviciosDB.sort((a, b) =>
+      a.label.localeCompare(b.label)
+    );
+
+    setData(infoOrdenada);
   }, [iServicios]);
 
   return (
